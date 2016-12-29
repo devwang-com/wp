14409a4810e46a4703b5822b2995375693792520
devwang-com <devwang.com@gmail.com>
2016-12-30 00:07:46 +0800

    diy wp from sae wp2

--- index.php
+++ index.php
@@ -1,17 +1,288 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>@devwang|个人主页</title>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+<link rel="shortcut icon" href="me/img/favicon.ico"/>
+<link rel="bookmark" href="me/img/favicon.ico"/>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<meta name="keywords" content="devwang" />
+<meta name="description" content="www.devwang.com" />
+<link href="./me/css/default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="./me/css/me.css" rel="stylesheet" type="text/css" media="all" />
+     <!-- 动态图 -->
+
+<script type="text/javascript" src="me/js/jquery.1.4.2-min.js"></script>
+     <!-- 动态图 -->
+<style type="text/css">
+
+
+
+
+
+</style>
+
+</head>
+
+ <!-- 
+ <audio autoplay="autoplay" loop="-1">   
+         <source src="./music/demo.mp3">   
+ </audio> 
+ -->  
+
+<div id="Nav">
+	<ul>
+        <li></li>     
+     	<li><a href="#" target="_blank" title="个人主页">&nbsp;&nbsp;HOME&nbsp;&nbsp;</a></li>
+        <li><a href="#" target="_blank" title="SUN">&nbsp;&nbsp;SUN&nbsp;&nbsp;</a></li>
+        <li><a href="#" target="_blank" title="MON">MON</a></li>
+        <li><a href="#" target="_blank" title="TUES">&nbsp;TUES&nbsp;</a></li>
+        <li><a href="#" target="_blank" title="WED">&nbsp;WED&nbsp;</a></li>
+        <li><a href="#" target="_blank" title="THUR">THUR</a></li>
+        <li><a href="#" target="_blank" title="FRI">FRI</a></li>
+        <li><a href="#" target="_blank" title="SAT">&nbsp;SAT&nbsp;</a></li>
+        <li><a href="#" target="_blank" title="关于博主">&nbsp;ABOUT&nbsp;</a></li>
+        <li></li>&nbsp;
+    </ul>
+
+</div>
+<div id="Banner">
+
+<div id="Logo">
+    <div class="content" id="one">
+    <div>
+        <div id="headbg">
+            <!-- 动态图 -->
+
+            <p align="center">
+ 				<div class="swipe">
+					<ul id="slider">
+						<li style="display:block">
+                  	     	 <img src="me/img/hutview.jpg" width="100%"/></li>
+                   		 <li><img src="me/img/hutview.jpg" width="100%"/></li>
+                   	 	
+					</ul>
+				<div id="pagenavi">
+					<a class="active"></a>
+					<a></a>
+					<a></a>
+				</div>
+			</div>
+   			<script type="text/javascript" src="me/js/touchScroll.js"></script>
+			<script type="text/javascript" src="me/js/touchslider.dev.js"></script>
+			<script type="text/javascript">
+			var active=0,
+			as=document.getElementById('pagenavi').getElementsByTagName('a');
+	
+			for(var i=0;i<as.length;i++){
+				(function(){
+					var j=i;
+					as[i].onclick=function(){
+					t2.slide(j);
+					return false;
+			}
+			})();
+			}
+
+			var t1=new TouchScroll({id:'wrapper','width':5,'opacity':0.7,color:'#555',minLength:20});
+
+			var t2=new TouchSlider({id:'slider', speed:600, timeout:3000, before:function(index){
+			as[active].className='';
+			active=index;
+			as[active].className='active';
+			}});
+			</script>
+            
+            
+             <!--动态图-->
+            
+            
+            <div class="top_content">
+                <p>&nbsp;</p>
+                <dl>
+                    <dt><a ><img src="me/img/devwang_com_circle_logo.jpg"></a></dt>
+                    <dd>
+                         <br/>
+                         <br/>
+                        <p>未来のために,一生厳命。</p>
+                    </dd>
+                </dl>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+
+</div>
+
+<!-- about me start-->    
+ <div id="header" class="container" >
+	<div id="logo">
+		<h1><a href="#">冬雷夏雪</a></h1>
+	</div>
+	<div id="menu">
+		<ul>
+		
+			<li class="current_page_item"><a href="http://www.devwang.com" accesskey="1" title="">首页</a></li>
+		
+		</ul>
+	</div>
+</div>
+<div id="banner">
+	<div class="container">
+		<ul class="staff">
+			
+			<li><img src="./me/img/sea.jpg" alt="" /></li>
+			
+		</ul>
+		<div class="title">
+			<h2>一只安静的程序猿</h2>
+			<span class="byline">
+			一无所有，只剩梦想。
+			<br/>
+			因为热爱，所以专注，把喜欢的事做到极致。
+			</span>
+		</div>
+		<p>9x年出生在天朝某个小山村<br />
+           小学的时候...<br />
+           初中的时候...<br />
+           高中的时候...<br />
+           大学的时候...<br />
+           工作的时候...<br /> 
+           现在......</p>
+		   <p>
+			兴趣:编程，黑科技，穷游名胜<span style="color:#263BD6">(~)</span><br/>
+			专长:android开发<br /> 
+			正在学习:android，日语
+		   </p>
+		 
+		<ul class="actions">
+			<li><a href="#1" class="button">往下有坎坷的人生</a></li>
+		</ul>
+	</div>
+</div>
+<div id="page" class="container" >
+	<div class="title">
+		<a  name="1">
+		<h2>灰色童年</h2></a>
+		<span class="byline">话灰色童年~~~<span style="color:#aa0000">（~~~）</span></span>
+	</div>
+	<p>
+	0<span style="color:#aa0000">（~~~）</span> 。<br/>
+	1<span style="color:#aa0000">（~~~）</span> 。<br/>
+	2<span style="color:#aa0000">（~~~）</span> 。
+    </p>
+	<ul class="actions">
+		<li><a href="#2" class="button">往下进入学生时代</a></li>
+	</ul>
+</div>
+<div id="featured">
+	<div class="container">
+		<div class="title">
+			<a name="2"><h2>学生时代</h2></a>
+			<span class="byline">此时代，中考！高考！！找工作！！！</span>
+		</div>
+		<p>
+		0<span style="color:#6359F7">(~~~)。</span><br/>
+        1<span style="color:#6359F7">(~~~)。</span><br/>
+        2<span style="color:#6359F7">(~~~)。</span>
+        </p>
+	</div>
+	<ul class="actions">
+		<li><a href="#3" class="button">往下职业生涯</a></li>
+	</ul>
+</div>
+<div id="extra" class="container">
+	<div class="title">
+		<a name="3"><h2>职业生涯</h2></a>
+		<br/>
+		<span class="byline">深圳</span>
+		<br/>
+	</div>
+	<div id="three-column">
+		<div class="boxA">
+			<div class="box">
+				<p>
+				职业目标:Android程序猿/攻城狮<br/>
+				码龄:1.x年<br/>
+				技能情况:android,linux,arm <br/>
+				联系方式:<a href= "mailto:devwang.com@gmail.com" style="text-decoration:none;color:#27AE61;"> 
+				devwang.com@gmail.com
+				</a>
+				<br/>
+				QQ群:214710126<br/>
+				<span style="color:#ff0000">没有对象怎么面向</span><span style="color:#6359F7">（你似不似傻，不会new一个啊）。</span><br/>
+				
+				</p>
+			</div>
+		</div>
+		<div class="boxB">
+			<div class="box">
+				<p> 陆游《钗头凤》</p><p>
+					红酥手，
+					黄籘酒，
+					满城春色宫墙柳。
+					东风恶，
+					欢情薄，
+					一怀愁绪，
+					几年离索。
+					错，错，错！
+					春如旧，
+					人空瘦，
+					泪痕红浥鲛绡透。	
+					桃花落，
+					闲池阁，
+					山盟虽在，
+					锦书难托。
+					莫，莫，莫！
+				</p>
+  
+			</div>
+		</div>
+		<div class="boxC">
+			<div class="box">
+				<p> 唐婉《钗头凤》</p><p>
+					世情薄，
+					人情恶，
+					雨送黄昏花易落。
+					晓风乾，
+					泪痕残，
+					欲笺心事，
+					独语斜栏。
+					难，难，难！
+					人成各，
+					今非昨，
+					病魂常似秋千索。
+					角声寒，
+					夜阑珊，
+					怕人寻问，
+					咽泪装欢。
+					瞒，瞒，瞒！
+				</p></div>
+		</div>
+	</div>
+	
+</div>
+ <!-- about me end-->  
+ 
+     
+	
+	<ul class="actions">
+		<li><a href="#" class="button">回到顶部</a></li>
+	</ul>
+    
+<div id="Footer">
+
+	版权所有&copy;冬雷夏雪(<a href="www.devwang.com" style="text-decoration:none;color:#fff;">www.devwang.com</a>)&nbsp;&copy;2016&nbsp;copyright&nbsp;by&nbsp;bruce&nbsp;and&nbsp;all&nbsp; rights &nbsp;reserved 
+<br/>
+&nbsp;email：<a href="mailto:devwang.com@gmail.com" style="text-decoration:none;color:#fff;">devwang.com@gmail.com</a> &nbsp; wechat：devwang_com
+
+
+</p>
+</div>
+
+ 
+    
+</body>
+</html>
